6928cfa78c7e36a2d3a7b565b974c914e796b5dc
Sichang Lee <titane308@gmail.com>
2017-07-17 23:07:54 +0900

    BT20 Finding Sreen Pixel Coordinates

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -33,7 +33,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	FVector HitLocation; //Out Parmeter
 	if (GetSightRayLocation(HitLocation)) //Has "side-effect", is going to line trace
 	{
-		UE_LOG(LogTemp, Warning, TEXT("Hit Location : %s"), *HitLocation.ToString());
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction : %s"), *HitLocation.ToString());
 		//TODO Tell controlled tank to aim at this point
 	}
 }
@@ -41,7 +41,14 @@ void ATankPlayerController::AimTowardsCrosshair()
 //Get world location of linetrace through crosshair if it hits the landscape,then get location.
 bool ATankPlayerController::GetSightRayLocation(FVector& OutHitLocation) const
 {
-	OutHitLocation = FVector(1.0);
+	// Find the crosshair position in pixel coordinates
+	int32 ViewportX, ViewportY;
+
+	GetViewportSize(ViewportX, ViewportY);
+	auto ScreenLocation = FVector2D(CrossHairXLocation*ViewportX, CrossHairYLocation*ViewportY);
+
+	// "De-project" the screen position of the crosshair to a world direction
+	// Line-trace along that look direction, and see what we hit ( up to max range)
 	return true;
 }
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -14,14 +14,20 @@ UCLASS()
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-
+private:
 	ATank* GetControlledTank() const;
 	
-	virtual void BeginPlay() override;
-	virtual void Tick(float DeltaTime) override;
-
 	// Start the tank moving the barrel so that a shot would hit where
 	// the crosshair interects the world
 	void AimTowardsCrosshair();
 	bool GetSightRayLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnyWhere)
+	float CrossHairXLocation = 0.5f;
+
+	UPROPERTY(EditAnyWhere)
+	float CrossHairYLocation = 0.3333f;
+public:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 };
