2b89d91af14ad3a08810e3752e075d1ee4b437b3
Sichang Lee <titane308@gmail.com>
2017-07-22 16:44:35 +0900

    BT28 Using FRotators in Unreal

--- BattleTank/Source/BattleTank/Public/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/TankAimingComponent.h
@@ -17,14 +17,8 @@ public:
 
 	void SetBarrelReference(UStaticMeshComponent* BarrelToSet);
 
-	// Called when the game starts
-	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
-
 	void AimAt(FVector HitLocation,float LaunchSpeed);
-
+	void MoveBarrelTowards(FVector AimDirection);
 private:
 	UStaticMeshComponent* Barrel = nullptr;
 	
