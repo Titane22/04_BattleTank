6f32555e4b9c2336738f2caa337467c2db9fa7d8
Titane22 <titane308@gmail.com>
2017-08-22 23:51:42 +0900

    BT71 OnComponentHit Event in 4.12

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -21,6 +21,11 @@ public:
 
 private:
 	UTankTrack();
+	
+	virtual void BeginPlay() override;
 
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
